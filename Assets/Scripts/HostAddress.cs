82ba4014a681a8a7a7b37aa09ebf961b4abdbeb8
Rahul Sinha <rahul.sinha1908@gmail.com>
2016-12-17 03:27:38 +0530

    Made the basic Networking And its Discovery Work

---
+++ Assets/Scripts/HostAddress.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class HostAddress : NetworkManager {
+
+	// Use this for initialization
+	NetworkManagerHUD netH;
+	public NetworkDiscovery discovery;
+	void Start () {
+		netH = GetComponent<NetworkManagerHUD> ();
+		LocalIPAddress ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OpenHost(){
+		//NetworkManager.singleton.networkAddress = "197.121.1.111";
+		NetworkManager.singleton.StartHost ();
+		netH.enabled = false;
+		discovery.broadcastData = "My Game";
+		discovery.Initialize ();
+		discovery.broadcastData = "My Game";
+		discovery.StartAsServer ();
+	}
+	public void ListenHost(){
+		discovery.Initialize ();
+		discovery.StartAsClient ();
+	}
+	public string LocalIPAddress()
+	{
+		IPHostEntry host;
+		string localIP = "";
+		host = Dns.GetHostEntry(Dns.GetHostName());
+		foreach (IPAddress ip in host.AddressList)
+		{
+			Debug.Log (ip);
+			if (ip.AddressFamily == AddressFamily.InterNetwork)
+			{
+				Debug.Log ("Internet " + ip);
+				localIP = ip.ToString();
+				break;
+			}
+		}
+		return localIP;
+	}
+}
---
+++ Assets/Scripts/NetworkDisc.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkDisc : NetworkDiscovery
+{
+
+	public override void OnReceivedBroadcast (string fromAddress, string data)
+	{
+		Debug.Log (data);
+		NetworkManager.singleton.networkAddress = fromAddress;
+		NetworkManager.singleton.networkPort = 7777;
+		NetworkManager.singleton.StartClient();
+	}
+
+}
