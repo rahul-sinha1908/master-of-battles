bcaf473aff9957446d2be6dd2061305eb499cc55
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-08 21:29:20 +0530

    Added the swapping option in the game.

--- Assets/Scripts/InputControl/MenuControls.cs
+++ Assets/Scripts/InputControl/MenuControls.cs
@@ -6,16 +6,21 @@ using MasterOfBattles;
 public class MenuControls : MonoBehaviour {
 
 	public float speed;
-	public GameObject mainmenu, searchForHost, productStore, trackLoc; 
+	public GameObject mainmenu, searchForHost, productStore, trackLoc, spawnLocation;
+	public GameObject[] myModels;
+	private int currentModel;
+	private GameObject currentSpawnedGO; 
 	private Camera cam;
 	
 	// Use this for initialization
 	void Start () {
 		speed=1;
 		cam=Camera.main;
+		currentModel=0;
 
 		trackLoc.transform.LookAt(mainmenu.transform);
 		cam.transform.rotation=trackLoc.transform.rotation;
+		currentSpawnedGO=GameObject.Instantiate(myModels[currentModel], spawnLocation.transform);
 	}
 	
 	public void goToMainMenu(){
@@ -30,6 +35,21 @@ public class MenuControls : MonoBehaviour {
 		trackLoc.transform.LookAt(productStore.transform);
 	}
 
+	public void changeModelRight(){
+		if(currentModel==myModels.Length-1)
+			return;
+		currentModel++;
+		Destroy(currentSpawnedGO);
+		currentSpawnedGO=GameObject.Instantiate(myModels[currentModel], spawnLocation.transform);
+	}
+	public void changeModelLeft(){
+		if(currentModel==0)
+			return;
+		
+		currentModel--;
+		Destroy(currentSpawnedGO);
+		currentSpawnedGO=GameObject.Instantiate(myModels[currentModel], spawnLocation.transform);
+	}
 	// Update is called once per frame
 	void Update () {
 		if(trackLoc.transform.rotation !=cam.transform.rotation){
